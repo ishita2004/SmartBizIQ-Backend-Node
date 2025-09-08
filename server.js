9526dc3346306811bb23f43b15b79fa59f558758
ishita2004 <ishitax2004@gmail.com>
2025-09-09 01:35:43 +0530

    Update server.js: Added CORS for deployed frontend

--- server.js
+++ server.js
@@ -15,7 +15,23 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // -------------------- Middlewares --------------------
-app.use(cors());
+const allowedOrigins = [
+  "https://smart-biz-iq-frontend1.vercel.app"
+];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin like mobile apps or curl
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `❌ The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload({
